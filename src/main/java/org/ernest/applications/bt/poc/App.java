a56b8d71db700ce595aef767206be9b3efbfad6f
Ernest Ortuño <ernest.ort.tor@gmail.com>
2016-02-20 21:26:18 +0100

    first commit

---
+++ src/main/java/org/ernest/applications/bt/poc/App.java
@@ -0,0 +1,13 @@
+package org.ernest.applications.bt.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
