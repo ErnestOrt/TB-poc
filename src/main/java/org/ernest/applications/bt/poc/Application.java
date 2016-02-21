6d4e7a5e4512ce5971ec589f4106de9a1ccdf0b7
Ernest Ortuño <ernest.ort.tor@gmail.com>
2016-02-21 02:02:37 +0100

    poc

--- src/main/java/org/ernest/applications/bt/poc/Application.java
+++ src/main/java/org/ernest/applications/bt/poc/Application.java
@@ -1,12 +1,22 @@
 package org.ernest.applications.bt.poc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
 public class Application 
 {
-	public static void main(String[] args) {
-        SpringApplication.run(Application.class);
-	}
+
+    @RequestMapping("/ping")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
